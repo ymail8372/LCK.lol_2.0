d526d3a9a4e2b4f13c639af9ab332822b60bcd54
ymail8372 <ymail8372@naver.com>
2024-01-18 21:18:18 +0900

    20240118_local

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@
 
 {
 date
-source "/home/ubuntu/myvenv/bin/activate"
-python3 /srv/LCK.lol_2.0/manage.py update
-} >> /srv/LCK.lol_2.0/log/update.log 2>&1
+# source "/home/ubuntu/myvenv/bin/activate"
+/Library/Frameworks/Python.framework/Versions/3.10/bin/python3 /Users/kimminseok/Library/Mobile\ Documents/com~apple~CloudDocs/Desktop/coding/LCKlol/manage.py update
+echo "-------------------------------------"
+} >> /Users/kimminseok/Library/Mobile\ Documents/com~apple~CloudDocs/Desktop/coding/LCKlol/log/update.log 2>&1
