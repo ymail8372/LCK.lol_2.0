7c39afda176ebd8b4c2e8c47d27214a0697286ae
Ubuntu <ubuntu@ip-172-31-44-166.ap-northeast-2.compute.internal>
2024-01-18 12:17:30 +0000

    20240118_aws

--- test.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-original_data = "abc"
-#utf8_encoded_data = original_data.encode('utf-8')
-#encoded_data = base64.b64encode(original_data)
-#print(utf8_encoded_data)
-print(type(original_data))
-#print(len(encoded_data))
-#print(encoded_data.decode('ASCII'))
