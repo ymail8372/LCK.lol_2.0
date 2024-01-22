69f7fa55f8f949627c6f23b32d68862c19590fb9
Ubuntu <ubuntu@ip-172-31-44-166.ap-northeast-2.compute.internal>
2024-01-22 20:39:42 +0900

    20240122_aws_0

--- update.sh
+++ update.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# update script
 # LCK.lol_2.0/update.sh
 
+filename=update_$(date +%y%m%d_%H:%M:%S).log
 {
-date
-/Library/Frameworks/Python.framework/Versions/3.10/bin/python3 /Users/kimminseok/Library/Mobile\ Documents/com~apple~CloudDocs/Desktop/coding/LCKlol/manage.py update
-echo "-------------------------------------"
-} >> /Users/kimminseok/Library/Mobile\ Documents/com~apple~CloudDocs/Desktop/coding/LCKlol/log/update.log 2>&1
-
+	date +%y/%m/%d_%H:%M:%S
+	source /home/ubuntu/myvenv/bin/activate
+	python3 /srv/LCK.lol_2.0/manage.py update
+} > "/srv/LCK.lol_2.0/log/$filename" 2>&1
