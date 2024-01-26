3b0d5bb8c4ccaa3f2db02ed0639c6ec79fa6c218
ymail8372 <ymail8372@naver.com>
2024-01-26 20:58:30 +0900

    void

--- index/management/commands/update.py
+++ index/management/commands/update.py
@@ -463,7 +463,7 @@ class Command(BaseCommand):
 					champion_object.save()
 			
 			# update last_update.txt
-            prev_matches.append(match_history)
+			prev_matches.append(match_history)
 			with open("/srv/LCK.lol_2.0/index/management/commands/last_update.txt", "w") as file :
 				file.write(f"{match_date.year}/{match_date.month}/{match_date.day}_{match_num}")
 
@@ -511,17 +511,10 @@ class Command(BaseCommand):
 				team2.save()
 
 	def handle(self, *args, **options):
-<<<<<<< HEAD
-		print("start to update schedule...")
-		self.update_schedule()
-		print("updating schedule complete!")
-		print("start to update champion...")
-=======
 		print("update schedule...")
 		self.update_schedule()
 		print("updating schedule complete!")
 		print("update champion...")
->>>>>>> ecbbba2e044977d49ac22968c0483820e969a97d
 		self.update_champion()
 		print("updating champion complete!")
 		print("start to update ranking_24_spring...")
