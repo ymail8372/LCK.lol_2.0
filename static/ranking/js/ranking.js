07eb04790acb764b2c379e4df761441f30862da4
ymail8372 <ymail8372@naver.com>
2024-05-21 23:24:56 +0900

    local cache remove

--- static/ranking/js/ranking.js
+++
@@ -1,9 +0,0 @@
-const player_ranking = document.querySelectorAll("#player_ranking_block table td:first-child");
-
-window.onload = function() {
-	player_ranking.forEach(function(ranking) {
-		if (ranking.textContent == "1") {
-			ranking.style.background = "#AADDFF";
-		}
-	});
-};
